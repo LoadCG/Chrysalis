9f5997c2b12c7644af9851c8fec5b94e5f717b9a
Cauan Gabriel <cauangabrielresende@gmail.com>
2024-09-09 21:48:58 -0300

    atualização 1
    atualização 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     
-<div id="alerts" class="bg-black mx-auto text-center h-8">
-        <span class="text-white">DESCONTOS IMPERDÍVEIS!</span>
-        <span class="text-white">ROUPAS DE QUALIDADE!</span>
+<div id="alerts" class="bg-white mx-auto text-center py-2 font-bold" style="user-select: none">
+        <span class="text-black mx-4">DESCONTOS IMPERDÍVEIS</span>
+        <span class="text-black mx-4">ROUPAS DE QUALIDADE</span>
 </div>
 <div id="default-carousel" class="relative w-full" data-carousel="slide">
     <!-- Carousel wrapper -->
@@ -22,22 +22,12 @@
         <div class="hidden duration-700 ease-in-out" data-carousel-item>
             <img src="assets/images/Logo_03.gif" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
-        <!-- Item 4 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="assets/images/Logo_04.gif" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-        <!-- Item 5 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="assets/images/Logo_05.gif" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
     </div>
     <!-- Slider indicators -->
     <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
         <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
     </div>
     <!-- Slider controls -->
     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -7,33 +7,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/assets/images/White_Butterfly.png" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
-    <title>Chrysalis</title>
-    <style>@keyframes butterfly-flying {
-        0%{transform: scaleX(1);}
-        50%{transform: scaleX(0.6);}
-        100%{transform: scaleX(1);}
-    }
-    
-    #butterfly_img{
-        transition: all 2s ease;
-    }
-    #butterfly_img:hover{
-        animation-name: butterfly-flying;
-        animation-duration: 0.8s;
-        animation-iteration-count: infinite;
-    }
+    <title>Chrysalis - Sua loja preferida</title>
+    <style>
+        @keyframes butterfly-flying {
+            0% {
+                transform: scaleX(1);
+            }
+
+            50% {
+                transform: scaleX(0.6);
+            }
+
+            100% {
+                transform: scaleX(1);
+            }
+        }
+
+        #butterfly_img {
+            transition: all 2s ease;
+        }
+
+        #butterfly_img:hover {
+            animation-name: butterfly-flying;
+            animation-duration: 0.8s;
+            animation-iteration-count: infinite;
+        }
     </style>
 </head>
 
 <body>
     <header>
 
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <nav class="bg-orange-500 border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div class="flex flex-wrap items-center space-x-3 rtl:space-x-reverse">
-                    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse"><img id="butterfly_img" src="assets/images/Black_Butterfly.png" class="h-8" alt="Chrysalis Logo Borboleta" /></a>
-                    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse"><img src="assets/images/Black_Chrysalis.png" class="h-8" alt="Chrysalis Logo Texto" /></a>
+                    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse"><img id="butterfly_img"
+                            src="assets/images/White_Butterfly.png" class="h-8" alt="Chrysalis Logo Borboleta" /></a>
+                    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse"><img
+                            src="assets/images/White_Chrysalis.png" class="h-8" alt="Chrysalis Logo Texto" /></a>
                 </div>
                 <button data-collapse-toggle="navbar-default" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -45,30 +58,33 @@
                             d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div class="hidden w-full md:block md:w-auto bg-orange-500" id="navbar-default">
                     <ul
-                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-orange-500 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-orange-500 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Início</a>
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white transition-colors hover:text-gray-900">Início</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sobre</a>
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white transition-colors hover:text-gray-900">Sobre</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Em
-                                promoção</a>
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white transition-colors hover:text-gray-900">Promo</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Ajuda</a>
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent text-white transition-colors hover:text-gray-900">Ajuda</a>
                         </li>
                         <li>
                             <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><img src="assets/images/icons/cart.svg" alt="Carrinho" class="fill-black"></a>
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent transition-colors hover:text-gray-900"><img src="assets/images/icons/cart.svg" alt="Carrinho" style="filter: invert(100%)"></a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 rounded md:hover:bg-transparent md:border-0 md:p-0 md:dark:hover:bg-transparent transition-colors hover:text-gray-900"><img src="assets/images/icons/account.svg" alt="Carrinho" style="filter: invert(100%)"></a>
                         </li>
                     </ul>
                 </div>
@@ -84,7 +100,9 @@
             <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span class="block text-sm text-gray-700 sm:text-center dark:text-gray-400">© 2024 <a href=""
                     class="hover:underline">Chrysalis™</a>. Todos os Direitos reservados.</span>
-        </div>
+                    <span class="block text-sm text-gray-700 sm:text-center dark:text-gray-400">Desenvolvidos por alunos do <a href="https://colegiocomercialcpv.com.br" target="_blank"
+                    class="hover:underline">Colégio Comercial de Caçapava</a>.</span>
+        </div>        
     </footer>
 
 
